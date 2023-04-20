0122d626cebcd0608da450a2f87eb428a7e0b2de
Gabriel Da Silva Cabral <gabriel.cabral@sede.telebras>
2023-04-20 13:31:40 -0300

    Upload de arquivos

---
+++ Módulo 40 Interface gráfica - Tkinter/aula02.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+
+janela = tk.Tk()
+
+janela.title('Cotação de moedas')
+
+mensagem = tk.Label(text='Sistema de busca de cotação de moedas', fg='white', bg='black', width=50, height=10)
+mensagem.pack()
+
+mensagem2 = tk.Label(text='Selecione a moeda desejada', height=15, width=70)
+mensagem2.pack()
+
+janela.mainloop()
