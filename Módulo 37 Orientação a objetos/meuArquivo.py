4d8d24b03df9aa31092be16bdebc77cfae4d7b5b
Gabriel Da Silva Cabral <gabriel.cabral@sede.telebras>
2023-05-04 15:38:36 -0300

    Upload de arquivos

--- Módulo 37 Orientação a objetos/meuArquivo.py
+++ Módulo 37 Orientação a objetos/meuArquivo.py
@@ -1,1 +1,17 @@
-print('Gabriel')
+nome = 'gabriel'
+print(nome.capitalize())
+
+class TV():
+    cor = 'preta'
+    tamanho = 55
+    canal = 10
+    volume = 50
+
+    def mudar_canal(self):
+        pass
+
+    def mudar_volume(self):
+        pass
+
+    def ligar_desligar(self):
+        pass
