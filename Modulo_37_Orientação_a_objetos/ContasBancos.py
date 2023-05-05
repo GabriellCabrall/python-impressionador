b79951f2c6e0eb7f301e608d20b7d8be1428756b
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-05-05 11:10:58 -0300

    Update ContasBancos.py

--- Modulo_37_Orientação_a_objetos/ContasBancos.py
+++ Modulo_37_Orientação_a_objetos/ContasBancos.py
@@ -1,10 +1,12 @@
 class ContaCorrente:
 
-    def __init__(self, nome, cpf):
+    def __init__(self, nome, cpf, agencia, num_conta):
         self.nome = nome
         self.cpf = cpf
         self.saldo = 0
         self.limite = None
+        self.agencia = agencia
+        self.num_conta = num_conta
 
     def consultar_saldo(self):
         print('Seu saldo atual é de R${:,.2f}'.format(self.saldo))
@@ -27,7 +29,7 @@ class ContaCorrente:
         print('Seu limite de cheque especial é de {:,.2f}'.format(self._limite_conta()))
 
 
-conta_Gabriel = ContaCorrente('Gabriel', '111.222.333-44')
+conta_Gabriel = ContaCorrente('Gabriel', '111.222.333-44', 1234, 34062)
 conta_Gabriel.consultar_saldo()
 
 # Depositando dinheiro na conta
@@ -35,7 +37,7 @@ conta_Gabriel.depositar(10000)
 conta_Gabriel.consultar_saldo()
 
 # Sacando dinheiro da conta
-conta_Gabriel.sacar_dinheiro(11001)
+conta_Gabriel.sacar_dinheiro(1100)
 
 print('Saldo final')
 conta_Gabriel.consultar_saldo()
