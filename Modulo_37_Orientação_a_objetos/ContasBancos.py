831cdeaf889420fcf36a09018848afbe6da20d81
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-05-08 08:52:08 -0300

    Adiciona função de histórico de transações

--- Modulo_37_Orientação_a_objetos/ContasBancos.py
+++ Modulo_37_Orientação_a_objetos/ContasBancos.py
@@ -1,5 +1,15 @@
+from datetime import datetime
+import pytz
+
+
 class ContaCorrente:
 
+    @staticmethod
+    def _data_hora():
+        fuso_BR = pytz.timezone('Brazil/East')
+        horario_BR = datetime.now(fuso_BR)
+        return horario_BR.strftime('%d/%m/%Y %H:%M:%S')
+    
     def __init__(self, nome, cpf, agencia, num_conta):
         self.nome = nome
         self.cpf = cpf
@@ -7,12 +17,14 @@ class ContaCorrente:
         self.limite = None
         self.agencia = agencia
         self.num_conta = num_conta
+        self.transacoes = []
 
     def consultar_saldo(self):
         print('Seu saldo atual é de R${:,.2f}'.format(self.saldo))
 
     def depositar(self, valor):
         self.saldo += valor
+        self.transacoes.append((valor, self.saldo, ContaCorrente._data_hora()))
 
     def _limite_conta(self):
         self.limite = -1000
@@ -24,16 +36,23 @@ class ContaCorrente:
             self.consultar_saldo()
         else:
             self.saldo -= valor
+            self.transacoes.append((-valor, self.saldo, ContaCorrente._data_hora()))
 
     def consultar_limite_chequeespecial(self):
         print('Seu limite de cheque especial é de {:,.2f}'.format(self._limite_conta()))
+    
+    def consultar_historico_transacoes(self):
+        print('Histórico de transações:')
+        print('Valor, Saldo, Data e Hora')
+        for transacao in self.transacoes:
+            print(transacao)
 
 
 conta_Gabriel = ContaCorrente('Gabriel', '111.222.333-44', 1234, 34062)
 conta_Gabriel.consultar_saldo()
 
 # Depositando dinheiro na conta
-conta_Gabriel.depositar(100000)
+conta_Gabriel.depositar(10000)
 conta_Gabriel.consultar_saldo()
 
 # Sacando dinheiro da conta
@@ -43,3 +62,6 @@ print('Saldo final')
 conta_Gabriel.consultar_saldo()
 
 conta_Gabriel.consultar_limite_chequeespecial()
+
+print('-' * 20)
+conta_Gabriel.consultar_historico_transacoes()
