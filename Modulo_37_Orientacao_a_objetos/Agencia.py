8503ade8444f41e7d220e998467c825ca6cd1c64
Gabriel da Silva Cabral <98562652+GabriellCabrall@users.noreply.github.com>
2023-05-09 08:41:16 -0300

    Cria subclasse AgenciaVirtual

--- Modulo_37_Orientacao_a_objetos/Agencia.py
+++ Modulo_37_Orientacao_a_objetos/Agencia.py
@@ -24,14 +24,25 @@ class Agencia:
         self.clientes.append((nome, cpf, patrimonio))
 
 
+class AgenciaVirtual(Agencia):
+    
+    def __init__(self, site, telefone, cnpj):
+        self.site = site
+        super().__init__(telefone, cnpj, 1000)
+        self.caixa = 1000000
+
+
+class AgenciaComum(Agencia):
+    pass
+
+
+class AgenciaPremium(Agencia):
+    pass
+
+
+
 agencia1 = Agencia(20203891, 2040281039, 3748)
 
-agencia1.caixa = 1000000
-
-agencia1.verificar_caixa()
-
-agencia1.emprestar_dinheiro(1500, 4340804444, 10)
-print(agencia1.emprestimos)
-
-agencia1.adicionar_cliente('Gabriel', 20837452157, 10000)
-print(agencia1.clientes)
+agencia_virtual = AgenciaVirtual('www.agenciavirtual.com', 22224444, 1921000000)
+agencia_virtual.verificar_caixa()
+print(agencia_virtual.clientes)
